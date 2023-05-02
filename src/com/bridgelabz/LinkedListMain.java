da58252d2047287027bace610f90e6a671e107a3
rajnishkr2308 <raj123rajnish2308@gmail.com>
2023-05-02 23:22:37 +0530

    Added:Adding by Linked list

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -10,5 +10,14 @@ package com.bridgelabz;
             linkedList.push(30);
             linkedList.push(70);
             linkedList.display();
+
+             /*
+         UC2: Simple Linked list by pushing 70,30,56.
+         */
+            LinkedList<Integer> linkedList1 = new LinkedList<>();
+            linkedList1.push(70);
+            linkedList1.push(30);
+            linkedList1.push(56);
+            linkedList1.display();
         }
     }
